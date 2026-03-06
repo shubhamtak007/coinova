290b4479be970effa6eb9f79e67022701972648b
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-03-06 12:27:02 +0530

    datatable component: broke the component into small function and datatable header bindings interface created

--- components/features/coins/data-table.tsx
+++ components/features/coins/data-table.tsx
@@ -1,26 +1,13 @@
-import { ColumnDef, useReactTable, getCoreRowModel, flexRender, Row } from '@tanstack/react-table';
-import { Spinner } from '@/components/ui/spinner';
-import { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuGroup, ContextMenuItem } from '@/components/ui/context-menu';
-import { useWindowVirtualizer } from '@tanstack/react-virtual';
 import { useRef } from 'react';
-import type { MenuItem } from '@/interfaces/data-table.interface';
-
-interface DataTableBindings<TData> {
-    list: TData[],
-    columns: ColumnDef<TData>[],
-    contextMenuList: MenuItem[],
-    listEmptyMessage?: string,
-    fetchingList?: boolean,
-    currentPageNumber?: number,
-    rowsPerPage?: number,
-    currentSortingValue: string | null,
-    sendSortingValueToParent: (key: string) => void,
-    onRowClicked: (row: Row<TData>) => void,
-    onContextMenuItemClicked: (row: Row<TData>, contextMenu: MenuItem, event: React.MouseEvent<HTMLElement>) => void
-}
+import { Spinner } from '@/components/ui/spinner';
+import { useWindowVirtualizer } from '@tanstack/react-virtual';
+import { useReactTable, getCoreRowModel, flexRender, Row } from '@tanstack/react-table';
+import { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuGroup, ContextMenuItem } from '@/components/ui/context-menu';
+import { DataTableBindings, DataTableHeaderBindings, DataTableBodyBindings } from '@/interfaces/data-table.interface';
 
 function DataTable<TData,>(bindings: DataTableBindings<TData>) {
     const { sendSortingValueToParent, onRowClicked, onContextMenuItemClicked, ...props } = bindings;
+    const tableWrapperRef = useRef<HTMLDivElement>(null);
 
     const tableConfig = useReactTable<TData>({
         data: props.list,
@@ -35,7 +22,6 @@ function DataTable<TData,>(bindings: DataTableBindings<TData>) {
         }
     });
 
-    const tableWrapperRef = useRef<HTMLDivElement>(null);
     const { rows } = tableConfig.getRowModel();
 
     const rowVirtualizer = useWindowVirtualizer({
@@ -45,146 +31,159 @@ function DataTable<TData,>(bindings: DataTableBindings<TData>) {
         overscan: 10
     })
 
-    const virtualItems = rowVirtualizer.getVirtualItems();
-    const paddingTop = virtualItems.length > 0 ? virtualItems[0].start : 0;
-    const paddingBottom = virtualItems.length > 0 ? rowVirtualizer.getTotalSize() - virtualItems[virtualItems.length - 1].end : 0;
-
     return (
         <>
             <div
                 ref={tableWrapperRef}
                 className="coins-sst-wrapper"
-                style={{
-                    overflow: 'auto',
-                    position: 'relative'
-                }}
             >
                 <table className="coins-server-side-table">
-                    <thead>
-                        {
-                            tableConfig.getHeaderGroups().map((headerGroup) => (
-                                <tr key={headerGroup.id}>
-                                    {
-                                        headerGroup.headers.map((header) => {
-                                            return (
-                                                <th
-                                                    key={header.id}
-                                                    className={header.column.columnDef.meta?.headerClassNames}
-                                                >
-                                                    {
-                                                        header.isPlaceholder ? null :
-                                                            flexRender(
-                                                                header.column.columnDef.header,
-                                                                header.getContext()
-                                                            )
-                                                    }
-                                                </th>
-                                            )
-                                        }
-                                        )
-                                    }
-                                </tr>
-                            ))
-                        }
-                    </thead>
+                    <DataTableHeader
+                        tableConfig={tableConfig}
+                    />
 
-                    {
-                        (props.fetchingList === false)
-                        &&
-                        <tbody style={{ position: 'relative' }}>
-                            {
-                                rows?.length > 0 ?
-                                    <>
-                                        {paddingTop > 0 && (
-                                            <tr style={{ border: 0 }}>
-                                                <td
-                                                    colSpan={props.columns.length}
-                                                    style={{ height: `${paddingTop}px`, padding: 0, border: 0, lineHeight: 0 }}
-                                                />
-                                            </tr>
-                                        )}
-
-                                        {virtualItems.map((virtualRow) => {
-                                            const row = rows[virtualRow.index] as Row<TData>
-                                            return (
-                                                (
-                                                    <ContextMenu
-                                                        key={virtualRow.key}
-                                                    >
-                                                        <ContextMenuTrigger asChild>
-                                                            <tr
-                                                                key={row.id}
-                                                                onClick={() => { onRowClicked(row) }}
-                                                                data-state={row.getIsSelected() && "selected"}
-                                                                data-index={virtualRow.index}
-                                                                ref={rowVirtualizer.measureElement}
-                                                            >
-                                                                {
-                                                                    row.getVisibleCells().map((cell) => (
-                                                                        <td
-                                                                            key={cell.id}
-                                                                            className={cell.column.columnDef.meta?.cellClassNames}
-                                                                        >
-                                                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                                                        </td>
-                                                                    ))
-                                                                }
-                                                            </tr>
-                                                        </ContextMenuTrigger>
-
-                                                        <ContextMenuContent>
-                                                            <ContextMenuGroup>
-                                                                {
-                                                                    props.contextMenuList.map((contextMenu) => {
-                                                                        return (
-                                                                            <ContextMenuItem
-                                                                                key={contextMenu.id}
-                                                                                onClick={(event) => onContextMenuItemClicked(row, contextMenu, event)}
-                                                                            >
-                                                                                {contextMenu.name}
-                                                                            </ContextMenuItem>
-                                                                        )
-                                                                    })
-                                                                }
-                                                            </ContextMenuGroup>
-                                                        </ContextMenuContent>
-                                                    </ContextMenu>
-                                                )
-                                            )
-                                        })}
-
-                                        {paddingBottom > 0 && (
-                                            <tr style={{ border: 0 }}>
-                                                <td
-                                                    colSpan={props.columns.length}
-                                                    style={{ height: `${paddingBottom}px`, padding: 0, border: 0, lineHeight: 0 }}
-                                                />
-                                            </tr>
-                                        )}
-                                    </>
-                                    :
-                                    <tr>
-                                        <td
-                                            colSpan={props.columns.length}
-                                            className="no-value-text"
-                                        >
-                                            {props.listEmptyMessage ? props.listEmptyMessage : 'No results'}
-                                        </td>
-                                    </tr>
-                            }
-                        </tbody>
-                    }
+                    <DataTableBody
+                        rows={rows}
+                        rowVirtualizer={rowVirtualizer}
+                        dataTableBindings={bindings}
+                    />
                 </table>
-            </div>
 
-            {
-                (props.fetchingList === true)
-                &&
-                <div className="place-items-center m-[20px]">
-                    <Spinner className="size-20" />
-                </div>
-            }
+                {
+                    (props.fetchingList === true)
+                    &&
+                    <div className="place-items-center m-[20px]">
+                        <Spinner className="size-20" />
+                    </div>
+                }
+            </div>
         </>
+    )
+}
+
+function DataTableHeader<TData,>({ tableConfig }: DataTableHeaderBindings<TData>) {
+    return (
+        <thead>
+            {
+                tableConfig.getHeaderGroups().map((headerGroup) => (
+                    <tr key={headerGroup.id}>
+                        {
+                            headerGroup.headers.map((header) => {
+                                return (
+                                    <th
+                                        key={header.id}
+                                        className={header.column.columnDef.meta?.headerClassNames}
+                                    >
+                                        {
+                                            header.isPlaceholder ? null :
+                                                flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )
+                                        }
+                                    </th>
+                                )
+                            }
+                            )
+                        }
+                    </tr>
+                ))
+            }
+        </thead>
+    )
+}
+
+function DataTableBody<TData,>({ rows, rowVirtualizer, dataTableBindings }: DataTableBodyBindings<TData>) {
+    const { onRowClicked, onContextMenuItemClicked, columns, listEmptyMessage, contextMenuList, fetchingList } = dataTableBindings;
+    const virtualItems = rowVirtualizer.getVirtualItems();
+    const paddingTop = virtualItems.length > 0 ? virtualItems[0].start : 0;
+    const paddingBottom = virtualItems.length > 0 ? rowVirtualizer.getTotalSize() -
+        virtualItems[virtualItems.length - 1].end : 0;
+
+    return (
+        fetchingList === false &&
+        <tbody style={{ position: 'relative' }}>
+            {
+                rows?.length > 0 ?
+                    <>
+                        {paddingTop > 0 && (
+                            <tr style={{ border: 0 }}>
+                                <td
+                                    colSpan={columns.length}
+                                    style={{ height: `${paddingTop}px`, padding: 0, border: 0, lineHeight: 0 }}
+                                />
+                            </tr>
+                        )}
+
+                        {virtualItems.map((virtualRow) => {
+                            const row = rows[virtualRow.index] as Row<TData>
+                            return (
+                                (
+                                    <ContextMenu
+                                        key={virtualRow.key}
+                                    >
+                                        <ContextMenuTrigger asChild>
+                                            <tr
+                                                key={row.id}
+                                                onClick={() => { onRowClicked(row) }}
+                                                data-state={row.getIsSelected() && "selected"}
+                                                data-index={virtualRow.index}
+                                                ref={rowVirtualizer.measureElement}
+                                            >
+                                                {
+                                                    row.getVisibleCells().map((cell) => (
+                                                        <td
+                                                            key={cell.id}
+                                                            className={cell.column.columnDef.meta?.cellClassNames}
+                                                        >
+                                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                        </td>
+                                                    ))
+                                                }
+                                            </tr>
+                                        </ContextMenuTrigger>
+
+                                        <ContextMenuContent>
+                                            <ContextMenuGroup>
+                                                {
+                                                    contextMenuList.map((contextMenu) => {
+                                                        return (
+                                                            <ContextMenuItem
+                                                                key={contextMenu.id}
+                                                                onClick={(event) => onContextMenuItemClicked(row, contextMenu, event)}
+                                                            >
+                                                                {contextMenu.name}
+                                                            </ContextMenuItem>
+                                                        )
+                                                    })
+                                                }
+                                            </ContextMenuGroup>
+                                        </ContextMenuContent>
+                                    </ContextMenu>
+                                )
+                            )
+                        })}
+
+                        {paddingBottom > 0 && (
+                            <tr style={{ border: 0 }}>
+                                <td
+                                    colSpan={columns.length}
+                                    style={{ height: `${paddingBottom}px`, padding: 0, border: 0, lineHeight: 0 }}
+                                />
+                            </tr>
+                        )}
+                    </>
+                    :
+                    <tr>
+                        <td
+                            colSpan={columns.length}
+                            className="no-value-text"
+                        >
+                            {listEmptyMessage ? listEmptyMessage : 'No results'}
+                        </td>
+                    </tr>
+            }
+        </tbody>
     )
 }
 
--- interfaces/data-table.interface.ts
+++ interfaces/data-table.interface.ts
@@ -1,6 +1,33 @@
+import { Virtualizer } from '@tanstack/react-virtual';
+import { Table, ColumnDef, Row } from '@tanstack/react-table';
+
 interface MenuItem {
     id: string,
     name: string
 }
 
-export type { MenuItem }
+interface DataTableBindings<TData> {
+    list: TData[],
+    columns: ColumnDef<TData>[],
+    contextMenuList: MenuItem[],
+    listEmptyMessage?: string,
+    fetchingList?: boolean,
+    currentPageNumber?: number,
+    rowsPerPage?: number,
+    currentSortingValue: string | null,
+    sendSortingValueToParent: (key: string) => void,
+    onRowClicked: (row: Row<TData>) => void,
+    onContextMenuItemClicked: (row: Row<TData>, contextMenu: MenuItem, event: React.MouseEvent<HTMLElement>) => void
+}
+
+interface DataTableHeaderBindings<TData> {
+    tableConfig: Table<TData>
+}
+
+interface DataTableBodyBindings<TData> {
+    rows: Row<TData>[],
+    rowVirtualizer: Virtualizer<Window, Element>,
+    dataTableBindings: DataTableBindings<TData>
+}
+
+export type { MenuItem, DataTableBindings, DataTableHeaderBindings, DataTableBodyBindings }
