f61663516b159f6455a9db9ca33afcf8d25ae91e
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-03-07 12:30:00 +0530

    overscan value updated

--- components/features/coins/data-table.tsx
+++ components/features/coins/data-table.tsx
@@ -28,7 +28,7 @@ function DataTable<TData,>(bindings: DataTableBindings<TData>) {
         count: rows.length,
         estimateSize: () => 60,
         measureElement: (el) => el.getBoundingClientRect().height,
-        overscan: 25
+        overscan: 30
     })
 
     return (
