15a5efbbffa29856f0ddae61d491dbb927d7f9fa
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-06-19 18:06:18 +0530

    again logs added

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,10 @@ export async function proxy(request: NextRequest) {
     const accessToken = request.cookies.get("accessToken");
     const refreshToken = request.cookies.get("refreshToken");
 
+    console.log("Proxy executed");
+    console.log("Access:", !!accessToken);
+    console.log("Refresh:", !!refreshToken);
+
     if (!accessToken && refreshToken) {
         const response = await fetch(
             `${process.env.NEXT_PUBLIC_API_URL}v0/auth/refresh-token`,
