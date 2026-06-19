8073f23b0e6870491ec0ef16b2975805d7c1944c
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-06-19 16:35:16 +0530

    proxy file updated

--- proxy.ts
+++ proxy.ts
@@ -5,34 +5,31 @@ export async function proxy(request: NextRequest) {
     const refreshToken = request.cookies.get("refreshToken");
 
     if (!accessToken && refreshToken) {
-        try {
-            const response = await fetch(
-                `${process.env.NEXT_PUBLIC_API_URL}v0/auth/refresh-token`,
-                {
-                    method: "POST",
-                    headers: {
-                        cookie: request.headers.get("cookie") ?? "",
-                    },
-                }
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}v0/auth/refresh-token`,
+            {
+                method: "POST",
+                headers: {
+                    cookie: request.headers.get("cookie") ?? "",
+                },
+            }
+        );
+
+        const redirectResponse = NextResponse.redirect(
+            request.nextUrl
+        );
+
+        const setCookies = response.headers.getSetCookie();
+
+        setCookies.forEach((cookie) => {
+            redirectResponse.headers.append(
+                "set-cookie",
+                cookie
             );
+        });
 
-            const nextResponse = NextResponse.next();
-
-            const setCookies = response.headers.getSetCookie();
-
-            setCookies.forEach((cookie) => {
-                nextResponse.headers.append("set-cookie", cookie);
-            });
-
-            return nextResponse;
-        } catch (error) {
-            console.error("Refresh failed", error);
-        }
+        return redirectResponse;
     }
 
     return NextResponse.next();
 }
-
-export const config = {
-    matcher: ["/((?!_next|favicon.ico).*)"],
-};
