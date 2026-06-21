5b4247237303c7e7da97a9d3353b88e500bfea74
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-06-21 17:00:07 +0530

    proxy file updated

--- proxy.ts
+++ proxy.ts
@@ -1,36 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest) {
-    const accessToken = request.cookies.get("accessToken");
-    const refreshToken = request.cookies.get("refreshToken");
+    // const accessToken = request.cookies.get("accessToken");
+    // const refreshToken = request.cookies.get("refreshToken");
 
-    if (!accessToken && refreshToken) {
-        try {
-            const response = await fetch(
-                `${process.env.NEXT_PUBLIC_API_URL}v0/auth/refresh-token`,
-                {
-                    method: "POST",
-                    headers: {
-                        cookie: request.headers.get("cookie") ?? "",
-                    },
-                }
-            );
+    // if (!accessToken && refreshToken) {
+    //     try {
+    //         const response = await fetch(
+    //             `${process.env.NEXT_PUBLIC_API_URL}v0/auth/refresh-token`,
+    //             {
+    //                 method: "POST",
+    //                 headers: {
+    //                     cookie: request.headers.get("cookie") ?? "",
+    //                 },
+    //             }
+    //         );
 
-            const redirectResponse = NextResponse.redirect(request.nextUrl);
-            const setCookies = response.headers.getSetCookie();
+    //         const redirectResponse = NextResponse.redirect(request.nextUrl);
+    //         const cookies = response.headers.getSetCookie();
 
-            setCookies.forEach((cookie) => {
-                redirectResponse.headers.append(
-                    "set-cookie",
-                    cookie
-                );
-            });
+    //         cookies.forEach((cookie) => {
+    //             redirectResponse.headers.append(
+    //                 "set-cookie",
+    //                 cookie
+    //             );
+    //         });
 
-            return redirectResponse;
-        } catch (error) {
-            console.error("Refresh token request failed:", error);
-        }
-    }
+    //         return redirectResponse;
+    //     } catch (error) {
+    //         console.error("Refresh token request failed:", error);
+    //     }
+    // }
 
-    return NextResponse.next();
+    // return NextResponse.next();
 }
