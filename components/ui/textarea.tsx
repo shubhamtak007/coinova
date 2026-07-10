a7a03f3fae5977b931e426dac0a8a288efcf1087
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-07-10 14:27:48 +0530

    input and textarea font size updated

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -12,7 +12,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
                 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive
                  dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2
                   text-base shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed
-                  disabled:opacity-50 md:text-sm`,
+                  disabled:opacity-50 text-[13px]`,
                 className
             )}
             {...props}
