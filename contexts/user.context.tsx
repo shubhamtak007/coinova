9af2cb4c94364aa9626a4391af61ec4035bca02c
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-08-02 14:02:57 +0530

    feat: show a message when the internet connection changes

--- contexts/user.context.tsx
+++ contexts/user.context.tsx
@@ -4,30 +4,43 @@ import { createContext, useContext, ReactNode, useState, SetStateAction, Dispatc
 import { User } from '@/interfaces/account-centre.interface';
 import { Spinner } from '@/components/ui/spinner';
 import { retrieveProfile } from '@/services/user.service';
+import { GlobeOff } from 'lucide-react';
+import axios, { AxiosError, isAxiosError } from 'axios';
 
 type UserContextProviderProps = {
     children: ReactNode,
     currentUser?: User
 }
 
+type Online = true | false;
+
 type UserContextType = {
     user: User | null,
-    setUser: Dispatch<SetStateAction<User | null>>;
+    setUser: Dispatch<SetStateAction<User | null>>,
+    isOnline: Online,
+    setIsOnline: Dispatch<SetStateAction<Online>>
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 const UserContextProvider = ({ children, currentUser }: UserContextProviderProps) => {
     const [user, setUser] = useState<User | null>(null);
+    const [isOnline, setIsOnline] = useState<Online>(navigator.onLine);
     const [fetchingDetails, setFetchingDetails] = useState<boolean>(true);
 
     useEffect(() => {
+        if (navigator.onLine === false) return;
+
         async function fetchUserDetails() {
             try {
                 const response = await retrieveProfile();
                 setUser(response.data.data);
-            } catch (error) {
+            } catch (error: unknown) {
+                if ((isAxiosError(error) && !error.response) || (error instanceof Error && error.message === 'Network Error')) {
+                    setIsOnline(false);
+                }
 
+                console.error(error);
             } finally {
                 setFetchingDetails(false);
             }
@@ -36,15 +49,28 @@ const UserContextProvider = ({ children, currentUser }: UserContextProviderProps
         fetchUserDetails();
     }, []);
 
-    return (
-        fetchingDetails ?
+    if (isOnline === false) {
+        return <div className="hz-and-vert-center flex items-center">
+            <GlobeOff />
+            <div className="text-[23px] ml-[8px]">You're offline</div>
+        </div>;
+    }
+
+    if (fetchingDetails) {
+        return (
             <div className="hz-and-vert-center">
                 <Spinner className="size-20" />
-            </div> :
-            <UserContext.Provider value={{ user, setUser }}>
-                {children}
-            </UserContext.Provider>
-    )
+            </div>
+        );
+    }
+
+    return (
+        <UserContext.Provider
+            value={{ user, setUser, isOnline, setIsOnline }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
 }
 
 const useUser = (): UserContextType => {
